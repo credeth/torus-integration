0b577cca64be7470936c1992ac40a705cbeb1caa
Saad Hassan <xeda@Saads-MacBook-Pro.local>
2019-08-03 16:39:09 +0530

    Fixes

--- src/helper.js
+++ src/helper.js
@@ -3,20 +3,31 @@ const contractAddress = '0x91609aD30EBa869C1F412E5656DeaD7bCaACaA5f';
 const contractABI = [{"constant":true,"inputs":[{"name":"x","type":"uint256"}],"name":"log_2","outputs":[{"name":"y","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"daoAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"DAO_CAP","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"INITIAL_REP","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"signal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_vouchee","type":"address"}],"name":"vouche","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getAllMembers","outputs":[{"name":"","type":"address[]"},{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_reputation","type":"uint256"}],"name":"issueReputation","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_of","type":"address"}],"name":"getReputation","outputs":[{"name":"reputation","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_rep","type":"uint256"}],"name":"daoDistribution","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"distributedByLockdrop","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"distributedByDao","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"LOCKDROP_CAP","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PER_DAY_VOUCHE_COUNT","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_daoAddress","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_vouchee","type":"address"},{"indexed":true,"name":"_voucher","type":"address"},{"indexed":false,"name":"_vouchedAmount","type":"uint256"}],"name":"Vouched","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_reputation","type":"uint256"}],"name":"DaoDistribution","type":"event"},{"anonymous":false,"inputs":[],"name":"Signaled","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_reputation","type":"uint256"}],"name":"IssueReputation","type":"event"}];
 const web3Obj = {
   web3: new Web3(),
-  setweb3: function() {
-    const web3Inst = new Web3(window.web3.currentProvider || 'ws://localhost:8546', null, {})
-    web3Obj.web3 = web3Inst
-    web3Obj.contract = new web3Obj.web3.eth.Contract(contractABI, contractAddress, {
-      gasPrice: '5000000000' // default gas price in wei, 5 gwei in this case
-    })
-    sessionStorage.setItem('pageUsingTorus', 'true')
-  },
   vouch: function(address) {
     web3Obj.contract.methods.vouche(address).send()
       .then(function(receipt) {
         console.log(receipt);
         return receipt;
       });
+  },
+  setweb3: function() {
+    //   console.log(Web3)
+    const web3Inst = new Web3(
+      window.web3.currentProvider || "ws://localhost:8546",
+      null,
+      {}
+    );
+    web3Obj.web3 = web3Inst;
+    sessionStorage.setItem("pageUsingTorus", "true");
+  },
+  setContractInstance: () => {
+    web3Obj.contract = new web3Obj.web3.eth.Contract(
+      contractABI,
+      contractAddress,
+      {
+        gasPrice: "5000000000" // default gas price in wei, 5 gwei in this case
+      }
+    );
   },
   getReputationHistory: async function(address) {
     let data = [];
